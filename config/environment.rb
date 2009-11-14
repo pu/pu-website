2840a091d5e364f05f4daedcb92efca85187e1db
Stefan Botzenhart <sb@basiszwo.com>
2009-11-14 11:37:51 +0100

    add compass, sass, haml and haml generator

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'haml'
+  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com'
   config.gem 'newrelic_rpm'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
