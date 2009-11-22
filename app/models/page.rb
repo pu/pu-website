ce957241a60de7cbbb040b581288a164ac7253db
Stefan Botzenhart <sb@basiszwo.com>
2009-11-22 14:04:39 +0100

    remove permalink from posts and pages; validate presence of category for posts;

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,14 +1,10 @@
 class Page < ActiveRecord::Base
-  
+    
   validates_presence_of :title, :body
   validates_uniqueness_of :title
   
-  #acts_as_textiled :body
+  acts_as_textiled :body
   
-  has_friendly_id :permalink, :use_slug => true, :strip_diacritics => true
-  
-  def permalink
-    self[:permalink] || title
-  end
+  has_friendly_id :title, :use_slug => true, :strip_diacritics => true
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,14 @@
 class Post < ActiveRecord::Base
-  
-  validates_presence_of :title, :body
+    
+  validates_presence_of :title, :body, :category
   validates_uniqueness_of :title
   
   belongs_to :category
   
-  #acts_as_textiled :body
+  acts_as_textiled :body
   
-  has_friendly_id :permalink, :use_slug => true, :strip_diacritics => true
+  has_friendly_id :title, :use_slug => true, :strip_diacritics => true
   
   named_scope :most_recent, :order => 'created_at DESC', :limit => '3'
-  
-  
-  def permalink
-    self[:permalink] || title
-  end
   
 end
