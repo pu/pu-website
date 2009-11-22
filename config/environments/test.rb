23f0bc228eb4edf0d8bf19bc2074b840207285fa
Stefan Botzenhart <sb@basiszwo.com>
2009-11-22 14:34:02 +0100

    raise delivery errors in development mode;

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,7 +19,6 @@ config.action_controller.allow_forgery_protection    = false
 
 # Tell Action Mailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 config.action_mailer.default_url_options = { :host => "uganda.local" }
 
