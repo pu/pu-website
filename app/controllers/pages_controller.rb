43a377e4fd5f4718e8dfe6740f57feab0189fcb7
Christian Buggle <cb@msales.com>
2010-03-08 12:44:24 +0100

    PagesController finds pages by title (not id).
    Page-show-view now robust against non-existant page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,8 @@ class PagesController < ApplicationController
   def show
     # page = params[:id].capitalize
     # @page = Page.find_by_title(page)
-    
-    @page = Page.find(params[:id])
-    page_title(@page.title) if @page    
-    
-    
+    # raise "id raise: #{params[:id]}"
+    @page = Page.find_by_title(params[:id])
+    page_title(@page.title) if @page
   end
 end
