0ce59bb62c7febf3414db88aebdbef70df9bfc41
Christian Buggle <cb@msales.com>
2010-03-17 19:23:04 +0100

    Add Phone To Parents.

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < ActiveRecord::Base
-  attr_accessible :name, :firstname, :street, :zip_code, :town, :email
+  attr_accessible :name, :firstname, :street, :zip_code, :town, :email, :phone
   
   has_many :parentships
   has_many :kids, :through => :parentships
