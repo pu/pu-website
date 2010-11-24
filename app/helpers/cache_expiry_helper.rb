1fe873607d511fbeb197d42b0a51332fe6b55984
Christian Buggle <cb@msales.com>
2010-11-24 17:36:50 +0100

    Consistent use of cache keys.

--- app/helpers/cache_expiry_helper.rb
+++ app/helpers/cache_expiry_helper.rb
@@ -7,21 +7,21 @@ module CacheExpiryHelper
   end
 
   def expire_view_cache_for_kid(kid)
-    expire_fragment(:key => "kids_row#{kid.id}")
+    expire_fragment(:key => "kids_row_#{kid.id}")
     kid.parentships.each{|p| expire_fragment(:key => "parentships_row_#{p.id}")}
     kid.parents.each{|p| expire_fragment(:key => "parents_row_#{p.id}")}
   end
 
   def expire_view_cache_for_parent(parent)
-    expire_fragment(:key => "parents_row#{parent.id}")
+    expire_fragment(:key => "parents_row_#{parent.id}")
     parent.parentships.each{|p| expire_fragment(:key => "parentships_row_#{p.id}")}
     parent.kids.each{|k| expire_fragment(:key => "kids_row_#{k.id}")}
   end
 
   def expire_view_cache_for_parentship(parentship)
     # This chould be model code, but conceptually it copes with views and caching
-    expire_fragment(:key => "kids_row#{parentship.kid.id}")
-    expire_fragment(:key => "parents_row#{parentship.parent.id}")
+    expire_fragment(:key => "kids_row_#{parentship.kid.id}")
+    expire_fragment(:key => "parents_row_#{parentship.parent.id}")
     expire_fragment(:key => "parentships_row_#{parentship.id}")
   end
 end
