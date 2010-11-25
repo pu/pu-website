0988fdd906def924b35fb01203005bb936151cb2
Christian Buggle <cb@msales.com>
2010-11-25 16:08:54 +0100

    Parents now require a name and a firstname.

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,7 +1,9 @@
 class Parent < ActiveRecord::Base
   attr_accessible :name, :firstname, :street, :zip_code, :town, :email, :phone
-  
+
   has_many :parentships
   has_many :kids, :through => :parentships
-  
+
+  validates_presence_of :firstname, :name
+
 end
