38e8758735a0ed4ad51f8cf53681049138152481
Christian Buggle <cb@msales.com>
2010-11-30 16:14:48 +0100

    Fix concatenation of parentships array.

--- app/controllers/parentships_controller.rb
+++ app/controllers/parentships_controller.rb
@@ -79,7 +79,7 @@ class ParentshipsController < ApplicationController
     kids = Kid.name_or_firstname_like(@search_text)
     parents = Parent.name_or_firstname_like(@search_text)
 
-    @parentships = kids.collect{|k| k.parentships } & parents.collect{|p| p.parentships}
+    @parentships = (kids.collect{|k| k.parentships } + parents.collect{|p| p.parentships}).uniq
     render :action => 'index'
   end
 
