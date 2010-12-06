c44f63ab821b7a5d81310f689f041ee5c6aee82b
Christian Buggle <cb@msales.com>
2010-12-06 10:56:58 +0100

    Retrieve kids order by number DESC from SQL, instead of reversing ordeer later.

--- app/helpers/parentships_helper.rb
+++ app/helpers/parentships_helper.rb
@@ -1,7 +1,7 @@
 module ParentshipsHelper
 
   def kids_select_for_parentship
-    Kid.find(:all, :order => "number").collect{ |k| [ "#{k.number} #{k.firstname} #{k.name}", k.id] }.reverse
+    Kid.find(:all, :order => "number DESC").collect{ |k| [ "#{k.number} #{k.firstname} #{k.name}", k.id] }
   end
 
   def parents_select_for_parentship
